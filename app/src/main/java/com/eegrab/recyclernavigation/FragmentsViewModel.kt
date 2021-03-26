48e678a3446c06e6f468fc0c92fc6d8e3d75960e
abhinesh <abhinesh.c@eegrab.com>
2021-03-26 14:55:19 +0530

    able to fetch data from api

--- app/src/main/java/com/eegrab/recyclernavigation/FragmentsViewModel.kt
+++ app/src/main/java/com/eegrab/recyclernavigation/FragmentsViewModel.kt
@@ -1,7 +1,25 @@
 package com.eegrab.recyclernavigation
 
+import android.content.Context
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.eegrab.mqtttest.ViewModel.MainViewModel
+import com.eegrab.mqtttest.ViewModel.MainViewModelFactory
+import com.eegrab.post_project.repository.Repository
 
 class FragmentsViewModel: ViewModel() {
+
+    lateinit var viewModel: MainViewModel
+
+
+    fun printName() {
+        //print("My name is abhinesh")
+        Log.d("Main", "My name is abhinesh: ")
+
+
+    }
 
 }
--- app/src/main/java/com/eegrab/recyclernavigation/UserList.kt
+++ app/src/main/java/com/eegrab/recyclernavigation/UserList.kt
@@ -6,18 +6,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.eegrab.mqtttest.ViewModel.MainViewModel
+import com.eegrab.mqtttest.ViewModel.MainViewModelFactory
+import com.eegrab.post_project.repository.Repository
+import com.google.gson.Gson
+import org.json.JSONObject
 
 
 class UserList : Fragment(R.layout.fragment_user_list) {
 
     val TAG = "Main"
 
-    lateinit var viewModel: FragmentsViewModel
+    lateinit var viewModelFragment: FragmentsViewModel
+
+    lateinit var viewModel: MainViewModel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         Log.d(TAG, "onViewCreated: is called")
 
-        viewModel = ViewModelProvider(this).get(FragmentsViewModel::class.java)
+        viewModelFragment = ViewModelProvider(this).get(FragmentsViewModel::class.java)
+
+        viewModelFragment.printName()
+
+        getUserDetails()
+    }
+
+    private fun getUserDetails() {
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+
+        try {
+            //viewModel.pushUserDetails("SAP00992892","abc")
+            viewModel.getUserDetails()
+        }
+        catch (e: Exception) {
+//            Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            Log.d("Main", e.message.toString())
+        }
+
+        viewModel.myResponse.observe(viewLifecycleOwner, Observer { response ->
+            if (response.isSuccessful) {
+                val jsonString = Gson().toJson(response.body())
+                val obj = JSONObject(jsonString)
+                
+                Log.d(TAG, response.body().toString())
+            }
+        })
     }
 }
